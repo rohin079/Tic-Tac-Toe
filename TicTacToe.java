9aa93bcacb81a5677e1de5352faca9047a5ce4fa
rohin079 <rohinmehrotra@gmail.com>
2022-09-29 22:48:40 +0530

    Added Main

--- TicTacToe.java
+++ TicTacToe.java
@@ -200,9 +200,81 @@ public class TicTacToe {
         System.out.println();
     }
     public static void main(String[] args) {
-        TicTacToe t1 = new TicTacToe();
-        t1.display();
+        String play;
+        Scanner sc = new Scanner(System.in);
+        int playerScore=0, computerScore=0;
+        System.out.print("\033[H\033[2J");
+        System.out.println("Game Rules:\nX will play first!");
+        do{
+            System.out.print("Choose- X O: ");
+            String ch=sc.next();
+            int turn=0,userChoice=0;
+            if(ch.toUpperCase().equals("X")){
+                userChoice=0;
+            }
+            else if(ch.toUpperCase().equals("O")){
+                userChoice=1;
+            }
+            TicTacToe T1 = new TicTacToe();
+            int winner=-1;
+            int count=0;
+            do{
+                if(T1.winnerCheck()=="X"){
+                    winner=0;
+                    break;
+                }
+                else if(T1.winnerCheck()=="O"){
+                    winner=1;
+                    break;
+                }
+                else{
+                    System.out.print("\033[H\033[2J");
+                    if(turn==userChoice){
+                        T1.display();
+                        System.out.print("Enter Position: ");
+                        int pos = sc.nextInt();
+                        while(!(T1.inputValidationCheck(pos, turn))){
+                            System.out.print("Position Occupied!\nChoose Again: ");
+                            pos = sc.nextInt();
+                        }
+                    }
+                    else{
+                        T1.computerMoves(turn);
+                    }
+                    count++;
+                    turn=(turn+1)%2;
+                }
+            }while(count!=9);
+            if(count == 9){
+                if(T1.winnerCheck()=="X"){
+                    winner=0;
+                }
+                else if(T1.winnerCheck()=="O"){
+                    winner=1;
+                }
+            }
+            System.out.print("\033[H\033[2J");
+            T1.display();
+            if(winner>-1){
+                if(winner==userChoice){
+                    System.out.println("You Won!");
+                    playerScore++;
+                }
+                else{
+                    System.out.println("Computer Won!");
+                    computerScore++;
+                }
+            }
+            else{
+                System.out.println("DRAW!");
+            }
+            System.out.print("Do you want to play another match? Y/N: ");
+            play=sc.next();
+            System.out.print("\033[H\033[2J");
+        }while(play.toUpperCase().equals("Y"));
+        System.out.println("\033[H\033[2JFinal Score!\nYou:"+playerScore+" Computer:"+computerScore);
+        sc.close();
     }
-    
 }
+
 
